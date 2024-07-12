7587fa9c9db4b8eb82d1cf2c2c69aa04300b24d2
A. Shafie <litenova@outlook.com>
2024-07-13 01:22:16 +0200

    Renamed registry extension methods to be more clear they are registering mongo configurations

--- src/Myrtle/Extensions/MongoConfigurationRegistryExtensions.cs
+++ src/Myrtle/Extensions/MongoConfigurationRegistryExtensions.cs
@@ -4,125 +4,125 @@ using Myrtle.Configurations;
 namespace Myrtle.Extensions;
 
 /// <summary>
-/// Provides extension methods for IMongoConfigurationRegistry to easily add specific configurations.
+/// Provides extension methods for IMongoConfigurationRegistry to easily register specific configurations.
 /// </summary>
 public static class MongoConfigurationRegistryExtensions
 {
     /// <summary>
-    /// Adds the DecimalConfiguration to the registry.
+    /// Registers the DecimalConfiguration to the registry.
     /// </summary>
-    /// <param name="registry">The IMongoConfigurationRegistry to add the configuration to.</param>
+    /// <param name="registry">The IMongoConfigurationRegistry to register the configuration to.</param>
     /// <returns>The IMongoConfigurationRegistry for method chaining.</returns>
-    public static IMongoConfigurationRegistry AddDecimalSerialization(this IMongoConfigurationRegistry registry)
+    public static IMongoConfigurationRegistry RegisterDecimalSerializationConfiguration(this IMongoConfigurationRegistry registry)
     {
         registry.Register<DecimalConfiguration>();
         return registry;
     }
 
     /// <summary>
-    /// Adds the EnumRepresentationConfiguration to the registry.
+    /// Registers the EnumRepresentationConfiguration to the registry.
     /// </summary>
-    /// <param name="registry">The IMongoConfigurationRegistry to add the configuration to.</param>
+    /// <param name="registry">The IMongoConfigurationRegistry to register the configuration to.</param>
     /// <returns>The IMongoConfigurationRegistry for method chaining.</returns>
-    public static IMongoConfigurationRegistry AddEnumRepresentation(this IMongoConfigurationRegistry registry)
+    public static IMongoConfigurationRegistry RegisterEnumRepresentationConfiguration(this IMongoConfigurationRegistry registry)
     {
         registry.Register<EnumRepresentationConfiguration>();
         return registry;
     }
 
     /// <summary>
-    /// Adds the IgnoreExtraElementsConfiguration to the registry.
+    /// Registers the IgnoreExtraElementsConfiguration to the registry.
     /// </summary>
-    /// <param name="registry">The IMongoConfigurationRegistry to add the configuration to.</param>
+    /// <param name="registry">The IMongoConfigurationRegistry to register the configuration to.</param>
     /// <returns>The IMongoConfigurationRegistry for method chaining.</returns>
-    public static IMongoConfigurationRegistry AddIgnoreExtraElements(this IMongoConfigurationRegistry registry)
+    public static IMongoConfigurationRegistry RegisterIgnoreExtraElementsConfiguration(this IMongoConfigurationRegistry registry)
     {
         registry.Register<IgnoreExtraElementsConfiguration>();
         return registry;
     }
 
     /// <summary>
-    /// Adds the ImmutableTypeConfiguration to the registry.
+    /// Registers the ImmutableTypeConfiguration to the registry.
     /// </summary>
-    /// <param name="registry">The IMongoConfigurationRegistry to add the configuration to.</param>
+    /// <param name="registry">The IMongoConfigurationRegistry to register the configuration to.</param>
     /// <returns>The IMongoConfigurationRegistry for method chaining.</returns>
-    public static IMongoConfigurationRegistry AddImmutableTypeSerialization(this IMongoConfigurationRegistry registry)
+    public static IMongoConfigurationRegistry RegisterImmutableTypeSerializationConfiguration(this IMongoConfigurationRegistry registry)
     {
         registry.Register<ImmutableTypeConfiguration>();
         return registry;
     }
 
     /// <summary>
-    /// Adds the GuidSerializationConfiguration to the registry.
+    /// Registers the GuidSerializationConfiguration to the registry.
     /// </summary>
-    /// <param name="registry">The IMongoConfigurationRegistry to add the configuration to.</param>
+    /// <param name="registry">The IMongoConfigurationRegistry to register the configuration to.</param>
     /// <returns>The IMongoConfigurationRegistry for method chaining.</returns>
-    public static IMongoConfigurationRegistry AddGuidSerialization(this IMongoConfigurationRegistry registry)
+    public static IMongoConfigurationRegistry RegisterGuidSerializationConfiguration(this IMongoConfigurationRegistry registry)
     {
         registry.Register<GuidSerializationConfiguration>();
         return registry;
     }
 
     /// <summary>
-    /// Adds the CamelCaseElementNameConvention to the registry.
+    /// Registers the CamelCaseElementNameConvention to the registry.
     /// </summary>
-    /// <param name="registry">The IMongoConfigurationRegistry to add the configuration to.</param>
+    /// <param name="registry">The IMongoConfigurationRegistry to register the configuration to.</param>
     /// <returns>The IMongoConfigurationRegistry for method chaining.</returns>
-    public static IMongoConfigurationRegistry AddCamelCaseElementNames(this IMongoConfigurationRegistry registry)
+    public static IMongoConfigurationRegistry RegisterCamelCaseElementNamesConfiguration(this IMongoConfigurationRegistry registry)
     {
         registry.Register<CamelCaseElementNameConvention>();
         return registry;
     }
 
     /// <summary>
-    /// Adds the IgnoreIfNullConvention to the registry.
+    /// Registers the IgnoreIfNullConvention to the registry.
     /// </summary>
-    /// <param name="registry">The IMongoConfigurationRegistry to add the configuration to.</param>
+    /// <param name="registry">The IMongoConfigurationRegistry to register the configuration to.</param>
     /// <returns>The IMongoConfigurationRegistry for method chaining.</returns>
-    public static IMongoConfigurationRegistry AddIgnoreIfNull(this IMongoConfigurationRegistry registry)
+    public static IMongoConfigurationRegistry RegisterIgnoreIfNullConfiguration(this IMongoConfigurationRegistry registry)
     {
         registry.Register<IgnoreIfNullConventionConfiguration>();
         return registry;
     }
 
     /// <summary>
-    /// Adds the StringObjectIdIdGeneratorConvention to the registry.
+    /// Registers the StringObjectIdIdGeneratorConvention to the registry.
     /// </summary>
-    /// <param name="registry">The IMongoConfigurationRegistry to add the configuration to.</param>
+    /// <param name="registry">The IMongoConfigurationRegistry to register the configuration to.</param>
     /// <returns>The IMongoConfigurationRegistry for method chaining.</returns>
-    public static IMongoConfigurationRegistry AddStringObjectIdGenerator(this IMongoConfigurationRegistry registry)
+    public static IMongoConfigurationRegistry RegisterStringObjectIdGeneratorConfiguration(this IMongoConfigurationRegistry registry)
     {
         registry.Register<StringObjectIdIdGeneratorConventionConfiguration>();
         return registry;
     }
 
     /// <summary>
-    /// Adds the UtcDateTimeSerializationConfiguration to the registry.
+    /// Registers the UtcDateTimeSerializationConfiguration to the registry.
     /// </summary>
-    /// <param name="registry">The IMongoConfigurationRegistry to add the configuration to.</param>
+    /// <param name="registry">The IMongoConfigurationRegistry to register the configuration to.</param>
     /// <returns>The IMongoConfigurationRegistry for method chaining.</returns>
-    public static IMongoConfigurationRegistry AddUtcDateTimeSerialization(this IMongoConfigurationRegistry registry)
+    public static IMongoConfigurationRegistry RegisterUtcDateTimeSerializationConfiguration(this IMongoConfigurationRegistry registry)
     {
         registry.Register<UtcDateTimeSerializationConfiguration>();
         return registry;
     }
 
     /// <summary>
-    /// Adds all available configurations to the registry.
+    /// Registers all available configurations to the registry.
     /// </summary>
-    /// <param name="registry">The IMongoConfigurationRegistry to add the configurations to.</param>
+    /// <param name="registry">The IMongoConfigurationRegistry to register the configurations to.</param>
     /// <returns>The IMongoConfigurationRegistry for method chaining.</returns>
-    public static IMongoConfigurationRegistry AddAllConfigurations(this IMongoConfigurationRegistry registry)
+    public static IMongoConfigurationRegistry RegisterAllConfigurations(this IMongoConfigurationRegistry registry)
     {
         return registry
-            .AddDecimalSerialization()
-            .AddEnumRepresentation()
-            .AddIgnoreExtraElements()
-            .AddImmutableTypeSerialization()
-            .AddGuidSerialization()
-            .AddCamelCaseElementNames()
-            .AddIgnoreIfNull()
-            .AddStringObjectIdGenerator()
-            .AddUtcDateTimeSerialization();
+            .RegisterDecimalSerializationConfiguration()
+            .RegisterEnumRepresentationConfiguration()
+            .RegisterIgnoreExtraElementsConfiguration()
+            .RegisterImmutableTypeSerializationConfiguration()
+            .RegisterGuidSerializationConfiguration()
+            .RegisterCamelCaseElementNamesConfiguration()
+            .RegisterIgnoreIfNullConfiguration()
+            .RegisterStringObjectIdGeneratorConfiguration()
+            .RegisterUtcDateTimeSerializationConfiguration();
     }
 }
